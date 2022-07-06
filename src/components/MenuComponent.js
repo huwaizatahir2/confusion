df2495124b63113aa2d6b49907fc30168151f2ff
Huwaiza <huwaiza.tahir@arbisoft.com>
2022-07-06 15:37:35 +0500

    card layout with did mount lifecycle on function component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,10 +1,46 @@
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+import { useState, useEffect } from 'react'
 
 function Menu(props){
+
+    const [selectedDish, setSelectedDish] = useState(null);
+
+    function onDishSelect(dish){
+        setSelectedDish(dish);
+    };
+    useEffect(() => {
+       console.log("component did mount") 
+    }, []);
+
+    function renderDish(dish){
+        if (dish != null){
+            return (
+                <Card>
+                    <CardImg width="100%" object src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>
+                            {dish.name}
+                        </CardTitle>
+                        <CardText>
+                            {dish.description}
+                        </CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return (
+                <div>
+
+                </div>
+            );
+        }
+    };
+
     const menu = props.dishes.map( (dish) => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card>
+                <Card onClick={() => onDishSelect(dish)}>  
                     <CardImg width="100%" object src={dish.image} alt={dish.name}/>
                     <CardImgOverlay>
                         <CardTitle>
@@ -20,6 +56,9 @@ function Menu(props){
         <div className="container">
             <div className="row">
                 {menu}
+            </div>
+            <div className="row">
+                {renderDish(selectedDish)}
             </div>
         </div>
     );
