24c8f8c75cc5a9be2ebb1ab10207b6e0d9bc4735
huwaiza tahir <huwaiza.tahir@arbisoft.com>
2022-07-26 16:40:04 +0500

    implemented home for SPA

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,12 +3,20 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { useState } from 'react'
 import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import { COMMENTS } from '../shared/comments';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 function Main() {
 
   const [dishes] = useState(DISHES);
+  const [promotions] = useState(PROMOTIONS);
+  const [leaders] = useState(LEADERS);
+  const [comments] = useState(COMMENTS);
+
   
   return (
     <div>
@@ -16,8 +24,13 @@ function Main() {
       <Routes>
         <Route path="/">
           <Route index element={<Home/>} />
-          <Route path='/home' element={<Home/>} />
-          <Route path='/menu' element={<Menu dishes={dishes} />} /> 
+          <Route path='/home' element={<Home dish={dishes.filter( (dish) => dish.featured)[0]} 
+              promotion={promotions.filter( (promo) => promo.featured)[0]}
+              leader={leaders.filter( (leader) => leader.featured)[0]}
+              />} 
+          />
+          <Route path='/menu' element={<Menu dishes={dishes} />} />
+          <Route path='/contactus' element={<Contact/>} />
           <Route
               path="*"
               element={<Navigate to="/home" replace />}
